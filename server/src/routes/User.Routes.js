7533d6b8050b6f3bcb5052e4b6b1ff7fdf65aa39
Satyamkus <satyam563gzp@gmail.com>
2025-07-26 21:07:34 +0530

    Fix: updated user login and register route paths

--- server/src/routes/User.Routes.js
+++ server/src/routes/User.Routes.js
@@ -10,8 +10,8 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const UserRouter = Router();
 
 
-UserRouter.route('/registeruser').post(registerUser);
-UserRouter.route('/loginuser').post(loginUser);
+UserRouter.route('/register').post(registerUser);
+UserRouter.route('/login').post(loginUser);
 UserRouter.post("/logout", verifyJWT, logoutUser);
 
 
