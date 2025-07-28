b2d22b72f9a75d9a3c6cc7b03895c837d1093e37
Akhil <achewadakhil11@gmail.com>
2025-07-29 02:35:15 +0530

    Added zod validators for login and signup

--- server/src/routes/User.Routes.js
+++ server/src/routes/User.Routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { registerUser, loginUser, logoutUser, updateResume, downloadResume } from '../controllers/User.Controller.js';
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { loginValidator, registerValidator } from "../middlewares/Validators.js";
 
 
 
@@ -10,8 +11,8 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const UserRouter = Router();
 
 
-UserRouter.route('/register').post(registerUser);
-UserRouter.route('/login').post(loginUser);
+UserRouter.route('/register').post(registerValidator,registerUser);
+UserRouter.route('/login').post(loginValidator,loginUser);
 UserRouter.post("/logout", verifyJWT, logoutUser);
 UserRouter.put("/updateresume/:id", verifyJWT, updateResume);
 UserRouter.route('/downloadresume/:id/download').get(verifyJWT, downloadResume);
